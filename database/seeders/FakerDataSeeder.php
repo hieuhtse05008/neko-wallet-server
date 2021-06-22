660ccd777ac98a095b736794930c9c37a313bf7e
Nang <goldenrezgun@gmail.com>
2021-06-22 19:29:49 +0700

    UPDATE: swap migration

--- database/seeders/FakerDataSeeder.php
+++ database/seeders/FakerDataSeeder.php
@@ -15,7 +15,17 @@ class FakerDataSeeder extends Seeder
      */
     public function run()
     {
-//        '[{"name":"1INCH","symbol":"1INCH","decimal":18,"icon_url":"https://d1j8r0kxyu9tj8.cloudfront.net/files/1622129149IZR7NaAU1G8UUfG.png","address":"0x111111111117dc0aa78b770fa6a738034120c302","network":"ERC20"},{"name":"Binance","symbol":"BNB","decimal":18,"icon_url":"https://d1j8r0kxyu9tj8.cloudfront.net/files/16221290772bs1foHrDTqNXei.png","address":"","network":"BEP20"},{"name":"Ethereum","symbol":"ETH","decimal":18,"icon_url":"https://d1j8r0kxyu9tj8.cloudfront.net/files/1622129114F0zZG66jYbSd3AT.png","address":"","network":"ERC20"},{"name":"Nami","symbol":"NAMI","decimal":18,"icon_url":"https://d1j8r0kxyu9tj8.cloudfront.net/files/1622124259bigVDhNIqQkwLOw.png","address":"0x2f7b618993cc3848d6c7ed9cdd5e835e4fe22b98","network":"ERC20"},{"name":"Attlas","symbol":"ATS","decimal":8,"icon_url":"https://d1j8r0kxyu9tj8.cloudfront.net/files/16221272798VlaMrrGypPIw45.png","address":"0xb9a6644bef37286fc08e703ecd15e9dedf78d3eb","network":"ERC20"},{"name":"Bami","symbol":"BAMI","decimal":18,"icon_url":"https://d1j8r0kxyu9tj8.cloudfront.net/files/1622129030nEQ30bKMIb0thmR.png","address":"0x8249bc1dea00660d2d38df126b53c6c9a733e942","network":"BEP20"},{"name":"Ola","symbol":"OLA","decimal":8,"icon_url":"https://d1j8r0kxyu9tj8.cloudfront.net/files/1622131725ELXs4viY5fRCM7f.jpeg","address":"0x47d0f6195911e93fe2b9b456289b6769aa47268f","network":"BEP20"},{"name":"KardiaChain","symbol":"KAI","decimal":18,"icon_url":"https://d1j8r0kxyu9tj8.cloudfront.net/files/1622212725ldYL7Vr38F2Gt0B.png","address":"0xD9Ec3ff1f8be459Bb9369b4E79e9Ebcf7141C093","network":"KRC20"},{"name":"My DeFi Pet","symbol":"DPET","decimal":18,"icon_url":"https://d1j8r0kxyu9tj8.cloudfront.net/files/1622285168YJTaqdbhtfi0bNo.png","address":"0xfb62AE373acA027177D1c18Ee0862817f9080d08","network":"KRC20"}]'
+        $dexes =[
+            [
+                "id" => Str::uuid()->toString(),
+                'name'=>'Binance',
+                'icon_url'=>'https://',
+            ]
+        ];
+
+        DB::table('dexes')->insertOrIgnore($dexes);
+
+
         $networks =  [
             "ERC20" => [
                  "id" => Str::uuid()->toString(),
