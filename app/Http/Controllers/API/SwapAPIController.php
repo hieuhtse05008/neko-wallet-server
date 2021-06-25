c66e9aa7aa50b20ad66c54ea865c347fa9fc224b
Phan Minh Duong <phanminhduong@hotmail.com>
2021-06-25 15:40:25 +0700

    ADD: swap address

--- app/Http/Controllers/API/SwapAPIController.php
+++ app/Http/Controllers/API/SwapAPIController.php
@@ -95,7 +95,7 @@ class SwapAPIController extends APIController
         $coin = $request->coin;
         $network = $request->network;
 
-        return response()->json(['address' => $api->depositAddress($coin, $network)['address']]);
+        return response()->json(['address' => $api->depositAddress($coin, $network)->addressa]);
     }
 
     /**
