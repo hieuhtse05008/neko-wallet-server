9282d07737d04c867a5193b4a86753d387551506
Nang <goldenrezgun@gmail.com>
2021-07-30 16:13:37 +0700

    CREATE: telegram news

--- resources/views/surf_news.blade.php
+++ resources/views/surf_news.blade.php
@@ -10,15 +10,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
             crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
-    <script>
-        _token = "{{ csrf_token() }}"
-    </script>
-    <script src="/js/surf_news.js?asdk2"></script>
 
     <title>News</title>
     <style>
@@ -205,6 +199,12 @@
 
 </div>
 
+<script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script>
+    _token = "{{ csrf_token() }}"
+</script>
+<script src="/js/surf_news.js?2342353wsfsf"></script>
 
 </body>
 </html>
