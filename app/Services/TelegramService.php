86f1797b861040ca14a188cefe37ce2cda2d6c6c
Nang <goldenrezgun@gmail.com>
2021-07-31 12:06:30 +0700

    CREATE: telegram news

--- app/Services/TelegramService.php
+++ app/Services/TelegramService.php
@@ -20,7 +20,7 @@ class TelegramService
         if (empty($chat_id)) return null;
         $httpClient = new \GuzzleHttp\Client();
 
-        $url = self::getQueryUrl('sendMessage?text='.$message.'&chat_id='.$chat_id.'&parse_mode='.$parse_mode.'');
+        $url = self::getQueryUrl('sendMessage?text='.$message.'&chat_id='.$chat_id.'&parse_mode='.$parse_mode.'&disable_web_page_preview=true');
 
         $response = $httpClient->post($url);
 //        $url = 'https://api.telegram.org/bot1811979499:AAFONyW26hIRGhndGLmERDuaLz-9MkKImzs/getMe';
