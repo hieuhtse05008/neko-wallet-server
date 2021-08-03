46e872c38c4320f411fb9b7553286a3b2e730bd1
Nang <goldenrezgun@gmail.com>
2021-08-03 15:46:29 +0700

    UPDATE: sync coingecko

--- app/Console/Commands/SyncCoinMarketsData.php
+++ app/Console/Commands/SyncCoinMarketsData.php
@@ -7,7 +7,7 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
-class SyncCoinMarktsData extends Command
+class SyncCoinMarketsData extends Command
 {
     /**
      * The name and signature of the console command.
@@ -46,12 +46,25 @@ class SyncCoinMarktsData extends Command
                 'total_volume'=>(string)$item['total_volume'],
                 'price_change_24h'=>(string)$item['price_change_24h'],
                 'price_change_percentage_24h'=>(string)$item['price_change_percentage_24h'],
+                'circulating_supply'=>(string)$item['circulating_supply'],
+                'total_supply'=>(string)$item['total_supply'],
+                'max_supply'=>(string)$item['max_supply'],
+                'market_cap_rank'=>(string)$item['market_cap_rank'],
+                'fully_diluted_valuation'=>(string)$item['fully_diluted_valuation'],
+                'high_24h'=>(string)$item['high_24h'],
+                'ath'=>(string)$item['ath'],
+                'ath_change_percentage'=>(string)$item['ath_change_percentage'],
+                'ath_date'=>(string)$item['ath_date'],
+                'low_24h'=>(string)$item['low_24h'],
+                'atl'=>(string)$item['atl'],
+                'atl_change_percentage'=>(string)$item['atl_change_percentage'],
+                'atl_date'=>(string)$item['atl_date'],
                 'last_updated'=>$item['last_updated'],
                 'created_at'=>$stamp,
                 'updated_at'=>$stamp,
             ];
         };
-        DB::connection($connection)->table('coin_markets_data')->insert($markets,'coin_id');
+        DB::connection($connection)->table('coin_markets_data')->insert($markets);
         return count($data);
     }
 
@@ -63,14 +76,14 @@ class SyncCoinMarktsData extends Command
     public function handle()
     {
         $stamp = now();
-        Log::info('start SyncCoinMarktsData');
+        Log::info('start SyncCoinMarketsData');
 
         $page = 0;
         while (($count = $this->getPage(++$page,$stamp)) > 0){
-            Log::info("$page SyncCoinMarktsData $count");
+            Log::info("$page SyncCoinMarketsData $count");
 
         }
-        Log::info('end SyncCoinMarktsData');
+        Log::info('end SyncCoinMarketsData');
         return 0;
     }
 }
