ec9d9bf73006fc1040f9c290d0bc78840c5decb7
Nang <goldenrezgun@gmail.com>
2021-08-30 18:55:26 +0700

    UPDATE: home.blade.php

--- app/Console/Commands/SyncCoinHistory.php
+++ app/Console/Commands/SyncCoinHistory.php
@@ -6,8 +6,8 @@ use App\Jobs\SyncCoinHistoryBinance;
 use App\Jobs\SyncCoinHistoryCoinGecko;
 use App\Jobs\SyncCoinHistoryHuobi;
 use App\Jobs\SyncCoinHistoryNami;
-use Illuminate\Support\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
@@ -44,45 +44,128 @@ class SyncCoinHistory extends Command
      */
     public function handle()
     {
+        $this->handleCoinMarketCaps();
+//        $this->handleCoinHistory();
+    }
+
+    private function handleCoinMarketCaps()
+    {
+
+        Log::info('start handleCoinMarketCaps');
+        $data = [];
+        for ($i = 1; $i <= 12000; $i++) {
+            $item = $this->handleCoinMarketCap($i);
+            if (!empty($item)) {
+                $data[] = $item;
+            }
+            usleep(900000);
+        }
+        $f = fopen("coins.json", "w") or die("Unable to open file!");
+        $txt = json_encode($data);
+        fwrite($f, $txt);
+        fclose($f);
+        Log::info('end handleCoinMarketCaps ' . count($data));
+        return 0;
+    }
+
+    private function handleCoinMarketCap($id)
+    {
+        echo "Id: $id" . "\xA";
+        Log::info("start handleCoinMarketCap $id");
+        try {
+            //get slug
+            $httpClient = new \GuzzleHttp\Client();
+            $url = "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/detail?id=$id";
+            $response = $httpClient->get($url);
+            $res = json_decode($response->getBody()->getContents())->data;
+
+            if (!property_exists($res, 'slug')) {
+                Log::info("FAIL $id handleCoinMarketCap");
+                return null;
+            }
+            $slug = $res->slug;
+            Log::info("end handleCoinMarketCap $id");
+
+
+            //get info
+            $content = (file_get_contents("https://coinmarketcap.com/currencies/$slug"));
+            preg_match("/<script id=\"__NEXT_DATA__\" type=\"application\/json\">(.*)<\/script>/", $content, $json, PREG_OFFSET_CAPTURE);
+            $text = (($json[0])[0]);
+            $dom = new \DOMDocument();
+            $dom->preserveWhiteSpace = false;
+            $dom->loadHTML($text, LIBXML_HTML_NOIMPLIED);
+            $dom->formatOutput = true;
+            $json_text = ($dom->textContent);
+
+            $obj = json_decode($json_text);
+            $platforms = object_get($obj, 'props.initialProps.pageProps.info.platforms');
+            $explorers = object_get($obj, 'props.initialProps.pageProps.info.urls.explorer');
+
+            echo json_encode([
+                'coin_market_cap_id' => $id,
+                'coin_market_cap_slug' => $slug,
+                'symbol' => $res->symbol,
+                'logo' => "https://s2.coinmarketcap.com/static/img/coins/200x200/$id",
+                'platforms' => $platforms,
+                'explorers' => $explorers,
+            ]). "\xA";
+        } catch (\Exception $e) {
+            Log::error($e);
+            return null;
+        }
+        return [
+            'coin_market_cap_id' => $id,
+            'coin_market_cap_slug' => $slug,
+            'symbol' => $res->symbol,
+            'logo' => "https://s2.coinmarketcap.com/static/img/coins/200x200/$id",
+            'platforms' => $platforms,
+            'explorers' => $explorers,
+        ];
+    }
+
+
+    private function handleCoinHistory()
+    {
         Log::info('start SyncCoinHistory');
         $connection = config('database.connections.warehouse.database');
-        for($i = 10000;$i<=11549; $i++){
-            $this->handleCoin($i,$connection);
+        for ($i = 1; $i <= 12000; $i++) {
+            $this->handleCoin($i, $connection);
         }
         Log::info('end SyncCoinHistory');
         return 0;
     }
 
-    private function handleCoin($id,$connection){
+    private function handleCoin($id, $connection)
+    {
         try {
             $httpClient = new \GuzzleHttp\Client();
             $url = "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/detail/chart?id=$id&range=ALL";
             $response = $httpClient->get($url);
             $res = json_decode($response->getBody()->getContents())->data;
-        }catch (\Exception $e){
+        } catch (\Exception $e) {
             Log::error($e);
             return;
         }
-        if(!property_exists($res,'points')) {
+        if (!property_exists($res, 'points')) {
             Log::info("FAIL $id SyncCoinHistory");
             return;
         }
         Log::info("$id SyncCoinHistory");
 
-         $res =   $res->points;
-        $timestamps=array_keys(get_object_vars($res));
+        $res = $res->points;
+        $timestamps = array_keys(get_object_vars($res));
         sort($timestamps);
         $data = [];
-        foreach ($timestamps as $timestamp){
+        foreach ($timestamps as $timestamp) {
             $v = $res->{$timestamp}->v;
- $data[] = [
-                'coin_market_cap_id'=>$id,
-                'price'=>$v[0],
-                'volume_24h'=>$v[1],
-                'market_cap'=>$v[2],
-                'coin_count'=>$v[4],
-                'created_at'=>Carbon::createFromTimestamp($timestamp)->toDateTimeString(),
-                'updated_at'=>Carbon::createFromTimestamp($timestamp)->toDateTimeString(),
+            $data[] = [
+                'coin_market_cap_id' => $id,
+                'price' => $v[0],
+                'volume_24h' => $v[1],
+                'market_cap' => $v[2],
+                'coin_count' => $v[4],
+                'created_at' => Carbon::createFromTimestamp($timestamp)->toDateTimeString(),
+                'updated_at' => Carbon::createFromTimestamp($timestamp)->toDateTimeString(),
             ];
         }
         DB::connection($connection)->table('temp_prices')->insert($data);
