6f746b4537d2100e48b0f79410ee1670428fe6c9
Nang <goldenrezgun@gmail.com>
2021-10-01 16:04:36 +0700

    UPDATE:  home.blade.php

--- app/Console/Commands/GetCoinMarketCapJson.php
+++ app/Console/Commands/GetCoinMarketCapJson.php
@@ -38,8 +38,8 @@ class GetCoinMarketCapJson extends Command
     {
 //        $this->handleCoin(11156);return;
 
-        for ($i = 11000; $i <= 14000; $i++) {
-//         foreach([] as $i){
+        for ($i = 9700; $i <= 14000; $i++) {
+//        foreach ([11424] as $i) {
             $data = $this->handleCoin($i);
             if ($data) $this->saveFile($i, $data);
             sleep(1);
@@ -63,28 +63,29 @@ class GetCoinMarketCapJson extends Command
             } else {
                 echo $id, PHP_EOL;
             }
+            return $api_data;
             //get info
-            $context = stream_context_create(
-                array(
-                    "http" => array(
-                        "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
-                    )
-                )
-            );
-            $content = (file_get_contents("https://coinmarketcap.com/currencies/{$api_data->slug}", false, $context));
-            $is_match = preg_match("/<script id=\"__NEXT_DATA__\" type=\"application\/json\">(.*)<\/script>/", $content, $json, PREG_OFFSET_CAPTURE);
-            if ($is_match != 1) return false;
-            $text = (($json[0])[0]);
-            $dom = new \DOMDocument();
-            $dom->preserveWhiteSpace = false;
-            $dom->loadHTML($text, LIBXML_HTML_NOIMPLIED);
-            $dom->formatOutput = true;
-            $json_text = ($dom->textContent);
-
-            $obj = json_decode($json_text);
-            $web_data = object_get($obj, 'props.initialProps.pageProps.info');
-
-            return (object)array_merge((array)$web_data, (array)$api_data);
+//            $context = stream_context_create(
+//                array(
+//                    "http" => array(
+//                        "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+//                    )
+//                )
+//            );
+//            $content = (file_get_contents("https://coinmarketcap.com/currencies/{$api_data->slug}", false, $context));
+//            $is_match = preg_match("/<script id=\"__NEXT_DATA__\" type=\"application\/json\">(.*)<\/script>/", $content, $json, PREG_OFFSET_CAPTURE);
+//            if ($is_match != 1) return false;
+//            $text = (($json[0])[0]);
+//            $dom = new \DOMDocument();
+//            $dom->preserveWhiteSpace = false;
+//            $dom->loadHTML($text, LIBXML_HTML_NOIMPLIED);
+//            $dom->formatOutput = true;
+//            $json_text = ($dom->textContent);
+//
+//            $obj = json_decode($json_text);
+//            $web_data = object_get($obj, 'props.initialProps.pageProps.info');
+//
+//            return (object)array_merge((array)$web_data, (array)$api_data);
         } catch (\Exception $e) {
             echo "FAIL $id ", $e, PHP_EOL;
             Log::info("FAIL CMC $id ");
@@ -102,6 +103,33 @@ class GetCoinMarketCapJson extends Command
         fclose($f);
     }
 
+    private function mergeFiles(){
+        $parse_down = new \Parsedown();
+        $parse_down->setSafeMode(true);
+        $cryptocurrencies = [];
+        for ($i = 11301; $i < 13000; $i++) {
+            $file_name = "{$this->path}/$i.json";
+            if(!file_exists($file_name)) continue;
+            $json_str = file_get_contents($file_name);
+            $data = json_decode($json_str);
+            if (!empty($data)) {
+                $cryptocurrencies[]=[
+                    'id'=>$data->id,
+                    'slug'=>$data->slug,
+                    'name'=>$data->name,
+                    'symbol'=>$data->symbol,
+                    'logo'=>"https://s2.coinmarketcap.com/static/img/coins/200x200/{$data->id}.png",
+                    'platforms'=>$data->platforms,
+                    'rank'=>object_get($data,'statistics.rank'),
+                ];
+            }
+        }
+        $f = fopen("cryptocurrencies.json", "w") or die("Unable to open file!");
+        $txt = json_encode($cryptocurrencies);
+        fwrite($f, $txt);
+        fclose($f);
+    }
+
     /**
      * Execute the console command.
      *
@@ -110,6 +138,7 @@ class GetCoinMarketCapJson extends Command
     public function handle()
     {
         $this->handleCoins();
+//        $this->mergeFiles();
 
         return 0;
     }
