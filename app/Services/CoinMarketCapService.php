b6d377939b605c8c7c8505cf1a2a1052ad0f14a3
Nang <goldenrezgun@gmail.com>
2021-10-02 10:28:37 +0700

    UPDATE:  home.blade.php

--- app/Services/CoinMarketCapService.php
+++ app/Services/CoinMarketCapService.php
@@ -6,15 +6,35 @@ namespace App\Services;
 
 class CoinMarketCapService
 {
-    private $api_key = 'e2b7a073-55a3-4e18-8a2a-61d77d029b75';
+    private $client;
+    public function __construct()
+    {
+        $this->client = new \GuzzleHttp\Client();
+    }
+
+    private  const api_key = 'e2b7a073-55a3-4e18-8a2a-61d77d029b75';
 
 
-    public function getCategories(){
-        $url ="https://pro-api.coinmarketcap.com/v1/cryptocurrency/categories?CMC_PRO_API_KEY={$this->api_key}";
+    public static function getCategories(){
+        $url ="https://pro-api.coinmarketcap.com/v1/cryptocurrency/categories?CMC_PRO_API_KEY=".self::api_key."";
         $httpClient = new \GuzzleHttp\Client();
         $response = $httpClient->get($url);
         dd($response->getBody()->getContents());
         $res = json_decode($response->getBody()->getContents());
+        return $res;
+    }
+
+    public static function getCryptocurrencies($filter=[
+        'listing_status'=> 'active',
+    ]){
+
+        $url ="https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=".self::api_key."";
+        foreach ($filter as $key=>$val){
+            $url .= "&$key=$val";
+        }
+        $httpClient = new \GuzzleHttp\Client();
+        $response = $httpClient->get($url);
+        $res = json_decode($response->getBody()->getContents())->data;
         return $res;
     }
 }
