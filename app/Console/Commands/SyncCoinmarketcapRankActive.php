9bfd682a56017d59e4b17b0668d1ff1862b80eba
Nang <goldenrezgun@gmail.com>
2021-10-02 09:55:58 +0700

    UPDATE: commands

---
+++ app/Console/Commands/SyncCoinmarketcapRankActive.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\CryptocurrencyMapping;
+use App\Services\CoinMarketCapService;
+use Illuminate\Console\Command;
+
+class SyncCoinmarketcapRankActive extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sync:rank_active';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    private $no_mappings = [];
+
+    private function update($filter)
+    {
+
+        $cryptocurrencies = CoinMarketCapService::getCryptocurrencies($filter);
+        foreach ($cryptocurrencies as $key => $item) {
+            $mapping = CryptocurrencyMapping::where('cmc_id', '=', $item->id)->first();
+            if (empty($mapping)) {
+                $this->no_mappings[] = $item->id;
+                echo "No mapping ", $item->id, PHP_EOL;
+                continue;
+            }
+            echo "Edit ", $item->id, ' ', $key, '/', count($cryptocurrencies), PHP_EOL;
+            $cryptocurrency = $mapping->cryptocurrency;
+            $cryptocurrency->rank = $item->rank;
+            $cryptocurrency->save();
+
+            $cryptocurrency_info = $cryptocurrency->cryptocurrency_info;
+            $cryptocurrency_info->status = $filter['listing_status'];
+            $cryptocurrency_info->save();
+        }
+
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->update(['listing_status' => 'active']);
+        $this->update(['listing_status' => 'inactive']);
+        $this->update(['listing_status' => 'untracked']);
+        dd($this->no_mappings);
+        return 0;
+    }
+}
