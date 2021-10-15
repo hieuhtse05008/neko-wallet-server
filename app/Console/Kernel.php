99ac4c7debbd9238e976088a36c248daf9f19f5e
Nang <goldenrezgun@gmail.com>
2021-10-15 18:45:11 +0700

    CREATE: RemoveUsedSwapOrder

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
 //         $schedule->command('inspire')->hourly();
+         $schedule->command('remove:used_swap_orders')->everyMinute();
     }
 
     /**
