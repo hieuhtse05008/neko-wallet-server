cd3583adae09f499b954cad91e98a87205bda83e
Nang <goldenrezgun@gmail.com>
2021-11-12 17:31:52 +0700

    UPDATE: blogs.blade.php

--- resources/views/web/nft/index.blade.php
+++ resources/views/web/nft/index.blade.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="utf-8"/>
-    <link rel="icon" href="/nft/neko-logo-orange.svg"/>
+    <link rel="icon" href="/images/logo/neko-logo-orange.svg"/>
     <meta name="viewport" content="width=device-width,initial-scale=1"/>
     <meta name="theme-color" content="#000000"/>
     <meta name="description" content="Web site created using create-react-app"/>
-    <link rel="manifest" href="/nft/manifest.json"/>
+    <link rel="manifest" href="/manifest.json"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>NEKO NFT</title>
@@ -106,6 +106,6 @@
         r()
     }([])</script>
 <script src="/nft/static/js/2.13c14de3.chunk.js"></script>
-<script src="/nft/static/js/main.51808dde.chunk.js"></script>
+<script src="/nft/static/js/main.28c8d171.chunk.js"></script>
 </body>
 </html>
