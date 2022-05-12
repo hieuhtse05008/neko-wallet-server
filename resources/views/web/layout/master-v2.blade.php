8ea99f7682360ff606b28276d832c099ec05b201
Nang <goldenrezgun@gmail.com>
2022-05-12 15:13:58 +0700

    Homepage v2

--- resources/views/web/layout/master-v2.blade.php
+++ resources/views/web/layout/master-v2.blade.php
@@ -5,7 +5,7 @@
 
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     @yield("meta")
-    <title>Neko Invest</title>
+    <title>Neko Wallet</title>
 
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
