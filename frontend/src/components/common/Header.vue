6663969f89282b37bc225aee78357dee80a36df1
trongnb <trong.nb121001@gmail.com>
2022-08-05 10:41:16 +0700

    ADD: Header

--- frontend/src/components/common/Header.vue
+++ frontend/src/components/common/Header.vue
@@ -8,7 +8,23 @@
     elevate-on-scroll
     scroll-target="#main"
   >
-    <v-btn color="primary" dark @click="handleLogout"> Logout </v-btn>
+    <v-row class="p-5">
+      <v-col cols="6">
+        <v-toolbar-title class="title">
+          <a href="/login">
+            <img
+              width="100"
+              height="28"
+              src="https://nekoinvest.io/images/logo/long-orange-text-black-neko.svg"
+              alt=""
+            />
+          </a>
+        </v-toolbar-title>
+      </v-col>
+      <v-col cols="6" class="text-right">
+        <v-btn color="primary" dark @click="handleLogout"> Logout </v-btn>
+      </v-col>
+    </v-row>
   </v-app-bar>
 </template>
 
@@ -36,5 +52,10 @@ export default {
 <style>
 .v-toolbar__content {
   justify-content: end;
+  padding: 10px;
+}
+.app-bar {
+  height: 60px !important;
+  padding: 10px 80px 0 80px;
 }
 </style>
