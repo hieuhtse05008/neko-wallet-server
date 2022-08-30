ff1932cf83bd805b8a4c2c7f63949b16858fcaa2
trongnb <trong.nb121001@gmail.com>
2022-08-30 15:11:10 +0700

    UPDATE: Responsive Detail blog page

--- resources/views/v3/blog/detail.blade.php
+++ resources/views/v3/blog/detail.blade.php
@@ -2,8 +2,10 @@
 @section('content')
 <section class="container">
     <div class="navigation-bar indent mb-4">
-        <i class="far fa-arrow-left me-2"></i>
-        <span class="fw-600 fs-16px me-4">All articles</span>
+        <a href="{{route('blogs')}}">
+            <i class="far fa-arrow-left me-2"></i>
+            <span class="fw-600 fs-16px me-4">All articles</span>
+        </a>
         <span class="fs-16px fw-400 opacity-60">{{($blog->created_at->format('d M, Y'))}}</span>
     </div>
     <div class="title indent mb-4">{{ $blog['title'] }}</div>
@@ -13,13 +15,13 @@
         <span class="me-2">-</span>
         <span>3 mins read</span>
     </div>
-    <div class="blog-image pb-5 mb-5"><img src="{{ $blog['image_url'] }}" alt=""></div>
+    <div class="blog-image pb-3 mb-3 pb-md-5 mb-md-5"><img src="{{ $blog['image_url'] }}" alt=""></div>
     <div class="ck-content content indent mb-5">
         <div class="share">
             <span class="fs-16px me-3">SHARE</span>
             <i class="fal fa-link icon me-2"></i>
             <i class="fal fa-paper-plane icon me-3"></i>
-            <i class="fab fa-facebook icon me-3 tooltip-element"></i>
+            <i class="fab fa-facebook icon me-3"></i>
             <i class="fab fa-twitter icon"></i>
         </div>
         {!! $blog['content'] !!}
@@ -125,5 +127,57 @@
         left: auto;
         bottom: auto;
     }
+
+    @media only screen and (max-width: 575px) {
+        .indent {
+            width: 95%;
+        }
+
+        .title {
+            font-size: 32px;
+            line-height: 42px;
+        }
+
+        .navigation-bar {
+            margin-top: 25px;
+        }
+
+        .share {
+            position: static;
+            margin-bottom: 22px;
+        }
+    }
+
+    /* Small devices (portrait tablets and large phones, 600px and up) */
+    @media only screen and (min-width: 576px) and (max-width: 767px) {
+        .indent {
+            width: 95%;
+        }
+
+        .title {
+            font-size: 50px;
+            line-height: 60px;
+        }
+
+        .navigation-bar {
+            margin-top: 50px;
+        }
+    }
+
+    /* Medium devices (landscape tablets, 768px and up) */
+    @media only screen and (min-width: 768px) and (max-width: 991px) {
+        .indent {
+            width: 95%;
+        }
+    }
+
+    /* Large devices (laptops/desktops, 992px and up) */
+    @media only screen and (min-width: 992px) and (max-width: 1199px) {
+        .indent {
+            width: 90%;
+        }
+    }
+
+    @media only screen and (min-width: 1200px) {}
 </style>
 @endsection
